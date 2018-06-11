4b043abc8efda12fde555f8eb918d433dbaf6c8e
Andrej Utz <>
2018-06-11 03:14:36 +0200

    Add forgotten files

---
+++ common/types.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+// integer
+using i8 = int8_t;
+using u8 = uint8_t;
+
+using i16 = int16_t;
+using u16 = uint16_t;
+
+using i32 = int32_t;
+using u32 = uint32_t;
+
+using i64 = int64_t;
+using u64 = uint64_t;
+
+// decimals
+using f32 = float;
+using f64 = double;
+
+// pointer size dependent
+using isz = ptrdiff_t;
+using usz = size_t;
