ab0d8dd31e77df7c4d7dc28d1398820bca6d2562
Andrej Utz <>
2018-06-11 05:57:54 +0200

    controller: add keyboard control with WASD

--- controller/controller.hpp
+++ controller/controller.hpp
@@ -9,6 +9,10 @@
 
 struct Controller
 {
+	enum Type
+	{
+		Joystick, Keyboard
+	};
 	enum Axis
 	{
 		LS_H = 0,
@@ -16,19 +20,28 @@ struct Controller
 		LT2 = 2,
 		RT2 = 5,
 	};
+	enum KeyState
+	{
+		Pressed, Released, Repeating
+	};
 
-	static constexpr i16 min = std::numeric_limits<i16>::min() + 1;
-	static constexpr i16 max = std::numeric_limits<i16>::max();
+	static constexpr i16 axis_min = std::numeric_limits<i16>::min() + 1;
+	static constexpr i16 axis_max = std::numeric_limits<i16>::max();
 
-	Controller(io_context& ctx, const char* dev_path);
+	Controller(io_context& ctx, Type type, const char* dev_path);
 
 	std::function<void(u32 time, Axis num, i16 val)> on_axis;
+	std::function<void(u32 time, u32 keycode, KeyState val)> on_key;
 
 private:
 	void recv_start();
-	void recv_handle(error_code ec, usz len);
+	void recv_handle_js(error_code ec, usz len);
+	void recv_handle_kb(error_code ec, usz len);
+
+	std::function<void(error_code ec, usz len)> recv_handle;
 
 	loggr logger;
+	Type type;
 	posix::stream_descriptor sd;
 	streambuf buf;
 };
