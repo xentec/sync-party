c4baa8878ba8161ef2b61b72a6706a5b8a7827c9
Andrej Utz <>
2018-06-11 12:29:23 +0200

    Reduce control scales for lower traffic

--- common/def.hpp
+++ common/def.hpp
@@ -9,13 +9,13 @@ constexpr unsigned short PORT = 4444;
 
 constexpr auto STEER_SUB = "sp/steer";
 constexpr Scale
-	STEER_SCALE { -900, 900 },
-	STEER_DC_SCALE { 1200000, 1800000 },
+    STEER_SCALE { -32, 32 },
+    STEER_DC_SCALE { 1200000, 1800000 },
 	STEER_DC_SCALE_CRIT { 500000, 2500000 };
 constexpr auto STEER_DC_DEF = STEER_DC_SCALE.min + (STEER_DC_SCALE.max - STEER_DC_SCALE.min)/2;
 constexpr auto STEER_DC_PERIOD = 20000000;
 
 constexpr auto MOTOR_SUB = "sp/motor";
-constexpr Scale MOTOR_SCALE { -200, 200 };
+constexpr Scale MOTOR_SCALE { -64, 64 };
 
 }
