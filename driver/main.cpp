3feb466387dddb1b263c044dd0f68872309affe5
Andrej Utz <>
2018-06-25 15:17:54 +0200

    driver: output distance samples in cm

--- driver/main.cpp
+++ driver/main.cpp
@@ -7,31 +7,33 @@
 #include <Arduino.h>
 #include <avr/wdt.h>
 
+#include <stdint.h>
+
 #define FW_VERSION 3
 
 #define MOTOR_PIN 11
 #define PING_TIMEOUT_MS 200
 #define PING_PIN 7
-#define US_TIMEOUT_MIS 2000
+#define US_TIMEOUT_MIS 18000
 
 
 // UltraSonic
 //############
-unsigned us_distance(int pin = PING_PIN)
+uint8_t us_distance(uint8_t pin = PING_PIN)
 {
-  // start
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(pin, LOW);
+	// start
+	pinMode(pin, OUTPUT);
+	digitalWrite(pin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(pin, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(pin, LOW);
 
-  // wait for echo
-  pinMode(pin, INPUT);
-  unsigned long dur = pulseIn(pin, HIGH, US_TIMEOUT_MIS); // 18500
+	// wait for echo
+	pinMode(pin, INPUT);
+	uint32_t dur = pulseIn(pin, HIGH, US_TIMEOUT_MIS); // 18500
 
-  return dur*100 / 29 / 2;
+	return dur / 58;
 }
 
 // Motor control
@@ -157,21 +159,11 @@ void handle()
 
 		switch(Type(type))
 		{
-		case Type::PING:
-			wd.reset();
-			break;
-		case Type::MOTOR:
-			motor::set_speed(data);
-			break;
-		case Type::ULTRA_SONIC:
-			data = us_distance(data);
-			break;
-		case Type::ANALOG:
-			data = map(analogRead(data), 0, 1023, 0, 255);
-			break;
-		case Type::VERSION:
-			data = FW_VERSION;
-			break;
+		case Type::PING:        wd.reset(); break;
+		case Type::VERSION:     data = FW_VERSION; break;
+		case Type::MOTOR:       motor::set_speed(data); break;
+		case Type::ULTRA_SONIC: data = us_distance(data); break;
+		case Type::ANALOG:      data = map(analogRead(data), 0, 1023, 0, 255); break;
 		default:
 			type |= ERR_BIT;
 			data = Error::INVALID_ARG;
