baff40f77bca2b03f9bcfea5a5efa72d8c5c2af4
Andrej Utz <>
2018-07-02 13:03:32 +0200

    camera: start refactoring

--- src/daemon/cortex/camera_opencv.h
+++ src/daemon/cortex/camera_opencv.h
@@ -1,55 +1,49 @@
-#ifndef CAMERA_OPENCV_H
-#define CAMERA_OPENCV_H
+#pragma once
 
-#include "opencv2/opencv.hpp"
-#include "opencv2/video.hpp"
-#include "opencv2/tracking/tracker.hpp"
-#include "zbar.h"
+#include "types.hpp"
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/tracking/tracker.hpp>
 #include <pthread.h>
-#include <iostream>
 #include <atomic>
 
-using namespace std;
-using namespace cv;
-using namespace zbar;
-
 class SyncCamera {
-private:
-    VideoCapture cap;
-    Mat input_template = imread("pattern.png",IMREAD_COLOR);
-    Mat tracking_image;
-    Rect2d tracking_rectangle;
-    Ptr<Tracker> tracker;
-    int tracker_is_initialized = 0;
-    int cam_fps = 0;
-    double matchval = 0.7;
+	cv::VideoCapture cap;
+	cv::Mat input_template;
+	cv::Mat tracking_image;
+	cv::Rect2d tracking_rectangle;
+	cv::Ptr<cv::Tracker> tracker;
+	int tracker_is_initialized = 0;
+	int cam_fps = 0;
+	double matchval = 0.7;
 
 public:
-    SyncCamera(int camera_number);
-    ~SyncCamera();
-    void set_resolution(int width, int height);
-    void set_pattern(String pattern);
-    void set_matchval(double value);
-    double PatternMatching_scaled(int match_method = CV_TM_CCOEFF_NORMED, int iterations = 20);
-    long ScanBarcode(int show_rectangle=0);
-    int PatternMatching(int match_method = CV_TM_CCOEFF_NORMED);
-    int TrackNext();
-    int InitializeTracker(string tracker_type = "MEDIANFLOW");
-    void FlushFrames(int seconds);
+	SyncCamera(int camera_number);
+	~SyncCamera();
+	void set_resolution(u32 width, u32 height);
+	void set_pattern(cv::String pattern);
+	void set_matchval(double value);
+	double pattern_matching_scaled(int match_method = CV_TM_CCOEFF_NORMED, int iterations = 20);
+	long scan_barcode(int show_rectangle=0);
+	int pattern_matching(int match_method = CV_TM_CCOEFF_NORMED);
+	int track_next();
+	int initialize_tracker(std::string tracker_type = "MEDIANFLOW");
+	void flush_frames(int seconds);
 };
 
 struct barcode_thread_data {
-    SyncCamera *cap = NULL;
-    int show_rectangle = 0;
-    int retries = 0; // 0 = infinite
+	SyncCamera *cap = NULL;
+	int show_rectangle = 0;
+	int retries = 0; // 0 = infinite
 };
 
 struct camera_thread_data {
-    SyncCamera *cap = NULL;
-    volatile int *returnvalue;
+	SyncCamera *cap = NULL;
+	volatile int *returnvalue;
 };
 
-void *ContinuousScanBarcode(void *thread_data);
-void *StartSyncCamera(void *thread_data);
-#endif
+void *continuous_scan_barcode(void *thread_data);
+void *start_sync_camera(void *thread_data);
+
 
