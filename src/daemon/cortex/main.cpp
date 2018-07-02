a2636c9b2ee66c3a2ccd9c25d629dba2a5420b04
xentec <xentec@aix0.eu>
2018-07-02 12:01:07 +0200

    cortex: adjust only as slave (-S)

--- src/daemon/cortex/main.cpp
+++ src/daemon/cortex/main.cpp
@@ -42,6 +42,7 @@ std::shared_ptr<T> try_init(const std::string& name, Args&& ...args)
 struct
 {
 	CommonOpts common;
+	bool is_slave;
 } conf;
 
 int main(int argc, const char* argv[])
@@ -53,6 +54,8 @@ int main(int argc, const char* argv[])
 
 	argh::parser opts(argc, argv);
 	parse_common_opts(opts, conf.common, false);
+
+	conf.is_slave = opts["-S"];
 
 	logger = slog::stdout_color_st("cortex");
 	logger->info("sp-cortex v0.1");
@@ -78,73 +81,77 @@ int main(int argc, const char* argv[])
 	} control_state;
 	std::unordered_map<std::string, std::function<void(std::string)>> fn_map;
 
-	if(driver)
+
+	fn_map.emplace(def::MOTOR_SUB, [&](const std::string& str)
 	{
-		fn_map.emplace(def::MOTOR_SUB, [&](const std::string& str)
+		i32 input = std::atoi(str.c_str());
+
+		u8 speed = proto::Speed::STOP;
+		if(input < 0) // NOTE: |[STOP, BACK_FULL]| != |[STOP, FORWARD_FULL]|
+			speed = map<u8>(input, def::MOTOR_SCALE.min, 0, proto::Speed::BACK_FULL, proto::Speed::STOP);
+		else if(input > 0)
+			speed = map<u8>(input, 0, def::MOTOR_SCALE.max, proto::Speed::STOP, proto::Speed::FORWARD_FULL);
+
+		if(control_state.speed != speed)
 		{
-			i32 input = std::atoi(str.c_str());
+			auto speed_corr = control_state.speed = speed;
 
-			u8 speed = proto::Speed::STOP;
-			if(input < 0) // NOTE: |[STOP, BACK_FULL]| != |[STOP, FORWARD_FULL]|
-				speed = map<u8>(input, def::MOTOR_SCALE.min, 0, proto::Speed::BACK_FULL, proto::Speed::STOP);
-			else if(input > 0)
-				speed = map<u8>(input, 0, def::MOTOR_SCALE.max, proto::Speed::STOP, proto::Speed::FORWARD_FULL);
+			if(conf.is_slave)
+				speed_corr = adjust_speed(control_state.steer_pwm, speed, control_state.gap, 0);
 
-			static u8 speed_prev = proto::Speed::STOP;
-			if(speed_prev != speed)
-			{
-				control_state.speed = speed_prev = speed;
-				auto speed_corr = adjust_speed(control_state.steer_pwm, speed, control_state.gap, 0);
-				logger->debug("HW: motor: {:02x} -> {:02x}", speed, speed_corr);
-
-				driver->drive(speed);
-			}
-		});
-
-		auto gap_timer = std::make_shared<recur_timer>(ioctx);
-		gap_timer->start(std::chrono::milliseconds(100), [&, gap_timer](auto ec)
-		{
-			if(ec) return;
-
-			static u8 pin = 7;
-
-			pin = pin == 8 ? 7 : 8;
+			logger->debug("HW: motor: {:02x} -> {:02x} - gap: {}", speed, speed_corr, control_state.gap);
 
 			if(driver)
-				driver->gap(pin, [&](auto ec, u8 cm)
+				driver->drive(speed);
+		}
+	});
+
+	auto gap_timer = std::make_shared<recur_timer>(ioctx);
+	gap_timer->start(std::chrono::milliseconds(100), [&, gap_timer](auto ec)
+	{
+		if(ec) return;
+
+		static u8 pin = 7;
+
+		pin = pin == 8 ? 7 : 8;
+
+		if(driver)
+			driver->gap(pin, [&](auto ec, u8 cm)
+			{
+				if(ec)
+					logger->debug("GAP ERR {} ", ec.message());
+				else
 				{
-					if(ec)
-						logger->debug("GAP ERR {} ", ec.message());
-					else
-					{
-						control_state.gap = cm;
-						logger->debug("GAP {} ", cm);
-					}
-				});
-		});
-	}
+					control_state.gap = cm;
+				}
+			});
+	});
+
 
 	if(steering)
 	{
 		steering->set_duty_cycle(def::STEER_DC_DEF);
 		steering->enable(true);
-		fn_map.emplace(def::STEER_SUB, [&](const std::string& str)
-		{
-			u32 pwm = map(std::atoi(str.c_str()),
-						  def::STEER_SCALE.min, def::STEER_SCALE.max,
-						  def::STEER_DC_SCALE.min, def::STEER_DC_SCALE.max);
-
-			static u32 pwm_prev = 0;
-			if(pwm_prev != pwm)
-			{
-				control_state.steer_pwm = pwm_prev = pwm;
-				auto pwm_corr = adjust_steer(pwm, control_state.gap);
-				logger->debug("HW: steer: {:7} -> {:7}", pwm, pwm_corr);
-
-				steering->set_duty_cycle(pwm_corr);
-			}
-		});
 	}
+
+	fn_map.emplace(def::STEER_SUB, [&](const std::string& str)
+	{
+		u32 pwm = map(std::atoi(str.c_str()),
+					  def::STEER_SCALE.min, def::STEER_SCALE.max,
+					  def::STEER_DC_SCALE.min, def::STEER_DC_SCALE.max);
+
+		if(control_state.steer_pwm != pwm)
+		{
+			auto pwm_corr = control_state.steer_pwm = pwm;
+			if(conf.is_slave)
+				pwm_corr = adjust_steer(pwm, control_state.gap);
+
+			logger->debug("HW: steer: {:7} -> {:7} - gap: {}", pwm, pwm_corr, control_state.gap);
+			if(steering)
+				steering->set_duty_cycle(pwm_corr);
+		}
+	});
+
 
 
 	struct {
@@ -176,7 +183,7 @@ int main(int argc, const char* argv[])
 		if(itr == fn_map.end())
 			return true;
 
-		logger->trace("data: {}[{}]: {}", topic_name, packet_id.get_value_or(-1), contents);
+		logger->trace("data: {}: {}", topic_name, contents);
 		itr->second(contents);
 
 		return true;
