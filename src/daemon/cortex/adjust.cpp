d7be1e88061879de688e880efc06496439e11e24
Trl34208 <tranluu.ngoc93@gmail.com>
2018-07-02 14:55:25 +0200

    Variable geaendert

--- src/daemon/cortex/adjust.cpp
+++ src/daemon/cortex/adjust.cpp
@@ -37,7 +37,7 @@ u8 adjust_speed(u32 steer, u8 speed, u8 gap_cm, int cam)
 
 	static i16 init_cam = cam;
 	if ((std::abs(init_cam - cam) <= 3)) {
-			new_speed = Werte[i].speed;
+			new_speed = speed;
 	}
 	else if(init_cam < cam)
 		new_speed -= 16;
