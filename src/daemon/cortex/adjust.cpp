707cc652f5f17344d7265862ac5ce40e69bd14ec
Andrej Utz <>
2018-07-02 12:13:33 +0200

    adjust: fix C constant

--- src/daemon/cortex/adjust.cpp
+++ src/daemon/cortex/adjust.cpp
@@ -7,19 +7,19 @@
 
 #define CAR_LENGTH 264
 #define CAR_WIDTH  195
-#define MINMAX_DEGREE 20
+#define MINMAX_DEGREE 20.0
 #define SPEED_DIFF 16
 #define TO_RADIANS 0.01745
 #define TO_DEGREES 57.2958
 
-constexpr f64 C = MINMAX_DEGREE * (def::STEER_DC_SCALE.max - def::STEER_DC_SCALE.min)/2.0;
+constexpr f64 C = MINMAX_DEGREE / (def::STEER_DC_SCALE.max - def::STEER_DC_SCALE.min) / 2;
 
 u8 adjust_speed(u32 steer, u8 speed, u8 gap_cm, int cam)
 {
 	u8 new_speed = speed;
 
 	const u32 gap_mm = gap_cm * 10 + CAR_WIDTH;
-	const f32 degree = std::abs(f32(def::STEER_DC_DEF - steer)) / C;
+	const f32 degree = std::abs(f32(def::STEER_DC_DEF - steer)) * C;
 	const f32 sin_w = 1 + (gap_mm * std::sin(degree*TO_RADIANS));
 
 	if(steer < def::STEER_DC_DEF)
@@ -42,7 +42,7 @@ u8 adjust_speed(u32 steer, u8 speed, u8 gap_cm, int cam)
 
 u32 adjust_steer(u32 steer, u8 gap_cm)
 {
-	const f32 degree = std::abs(f32(def::STEER_DC_DEF - steer)) / C;
+	const f32 degree = std::abs(f32(def::STEER_DC_DEF - steer)) * C;
 
 	const f32 r2 = (CAR_LENGTH / std::sin(degree * TO_RADIANS)) + CAR_WIDTH + gap_cm * 10;
 	f32 corr = 15000 * std::asin(CAR_LENGTH / r2) * TO_DEGREES;
